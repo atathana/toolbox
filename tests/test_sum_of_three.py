3883de093d9fbdc782b74fd5d919aacac440d6d7
Nasia Athanasiadou <nasioulita@gmail.com>
2022-01-29 15:59:06 +0100

    Initial Push

---
+++ tests/test_sum_of_three.py
@@ -0,0 +1,5 @@
+# pylint: disable-all
+from toolbox.sum_of_three import sum_of_three
+
+def test_sum_of_three():
+    assert sum_of_three(0, 0, 0) == 0
---
+++ toolbox/sum_of_three.py
@@ -0,0 +1,10 @@
+# pylint: disable=missing-module-docstring
+
+
+def sum_of_three(first_number, second_number, third_number):
+    """
+    Takes three numbers and returns the sum of those numbers
+    """
+    # $CHALLENGIFY_BEGIN
+    return first_number + second_number + third_number
+    # $CHALLENGIFY_END
